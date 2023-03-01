f04295baa8db3adce8196ca2a93e256a9cab7192
John Nicholas <john.nicholas@digital.cabinet-office.gov.uk>
2023-03-01 17:01:20 +0000

    more stats

--- src/simulator.spec.ts
+++ src/simulator.spec.ts
@@ -1,51 +1,94 @@
-import { simulate } from './simulator'
+import { simulate, findMedian } from './simulator'
+
+describe('median calculator will', () => {
+  test('return undefined with no iteam', () => {
+    expect(findMedian([])).toEqual(NaN)
+  })
+
+  test('return item with single iteam', () => {
+    expect(findMedian([12])).toEqual(12)
+  })
+
+  test('return middle item with odd number', () => {
+    expect(findMedian([12, 58, 100])).toEqual(58)
+  })
+
+  test('return mean of middle 2 numbers if even number of items', () => {
+    expect(findMedian([12, 58, 70, 100])).toEqual((58 + 70) / 2)
+  })
+
+  test('the dataset input keeps the corect order', () => {
+    const input = [12, 70, 58, 100]
+    findMedian(input)
+    expect(input).toEqual([12, 70, 58, 100])
+  })
+})
 
 describe('simulator with 100% success', () => {
   test('one task will take time of task', () => {
     const tasks = [{ time: 100, failure: 0, name: 'dev' }]
-    expect(simulate(tasks, 100).total).toBe(100);
+    const result = simulate(tasks, 100);
+    expect(result.mean).toBe(100);
+    //expect(result.breakdowns).toEqual([{ name: 'dev', total: 100 }])
+    expect(result.median).toBe(100);
   })
 
   test('two tasks will be total', () => {
     const tasks = [{ time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0, name: 'test' }]
-    expect(simulate(tasks, 100).total).toBe(200);
+    expect(simulate(tasks, 100).mean).toBe(200);
+    expect(simulate(tasks, 100).median).toBe(200);
   })
 })
 
 describe('simulator with 50% success', () => {
+  const iterations = 1000000
   test('2 tasks task will a suprining amount of time', () => {
     const tasks = [{ time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0.5, name: 'test' }]//, { time: 100, failure: 0.5 }, { time: 100, failure: 0.5 }]
-    const result = simulate(tasks, 1000000)
-    expect(result.total).toBeGreaterThan(395);
-    expect(result.total).toBeLessThan(405);
+    const result = simulate(tasks, iterations)
+    expect(result.mean).toBeGreaterThan(395);
+    expect(result.mean).toBeLessThan(405);
+    expect(result.median).toBeGreaterThanOrEqual(200);
+    expect(result.median).toBeLessThanOrEqual(400);
+    expect(result.mode).toBe(200);
   })
 
   test('3 tasks task will a suprining amount of time', () => {
     const tasks = [{ time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0.5, name: 'test' }, { time: 100, failure: 0.5, name: 'nearest' }]//, { time: 100, failure: 0.5 }]
-    const result = simulate(tasks, 1000000)
-    expect(result.total).toBeGreaterThan(995);
-    expect(result.total).toBeLessThan(1005);
+    const result = simulate(tasks, iterations)
+    expect(result.mean).toBeGreaterThan(995);
+    expect(result.mean).toBeLessThan(1005);
+    expect(result.median).toBeGreaterThanOrEqual(700);
+    expect(result.median).toBeLessThanOrEqual(800);
+    expect(result.mode).toBe(300);
   })
 
   test('4 tasks task will a suprining amount of time', () => {
     const tasks = [{ time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0.5, name: 'test' }, { time: 100, failure: 0.5, name: 'nearest' }, { time: 100, failure: 0.5, name: 'int' }]
-    const result = simulate(tasks, 1000000)
-    expect(result.total).toBeGreaterThan(2195);
-    expect(result.total).toBeLessThan(2205);
+    const result = simulate(tasks, iterations)
+    expect(result.mean).toBeGreaterThan(2195);
+    expect(result.mean).toBeLessThan(2205);
+    expect(result.median).toBe(1600);
+    expect(result.mode).toBe(400);
   })
 
   test('2 tasks reversed  will a  less suprining amount of time', () => {
     const tasks = [{ time: 100, failure: 0.5, name: 'dev' }, { time: 100, failure: 0, name: 'test' }]//, { time: 100, failure: 0.5 }, { time: 100, failure: 0.5 }]
-    const result = simulate(tasks, 1000000)
-    expect(result.total).toBeGreaterThan(295);
-    expect(result.total).toBeLessThan(305);
+    const result = simulate(tasks, iterations)
+    expect(result.mean).toBeGreaterThan(295);
+    expect(result.mean).toBeLessThan(305);
+    expect(result.median).toBeGreaterThanOrEqual(200);
+    expect(result.median).toBeLessThanOrEqual(300);
+    expect(result.mode).toBe(200);
   })
 
   test('4 tasks first reversed rest halved  will a suprining amount of time', () => {
     const tasks = [{ time: 100, failure: 0.5, name: 'dev' }, { time: 100, failure: 0, name: 'test' }, { time: 100, failure: 0.25, name: 'nearest' }, { time: 100, failure: 0.25, name: 'int' }]
-    const result = simulate(tasks, 1000000)
-    expect(result.total).toBeGreaterThan(843);
-    expect(result.total).toBeLessThan(853);
+    const result = simulate(tasks, iterations)
+    expect(result.mean).toBeGreaterThan(843);
+    expect(result.mean).toBeLessThan(853);
+    expect(result.median).toBeGreaterThanOrEqual(700);
+    expect(result.median).toBeLessThanOrEqual(800);
+    expect(result.mode).toBe(400);
   })
 
 
--- src/simulator.ts
+++ src/simulator.ts
@@ -1,44 +1,124 @@
 export interface Task {
   time: number
   failure: number
+  name: string
 }
 
-export function simulate(tasks: Task[], iterations: number) {
-  const results: number[] = []
+export type Breakdown = {
+  [key: string]: number
+}
+
+export interface SimulateResult {
+  mean: number
+  median: number
+  mode: number
+  iterations: number
+}
+
+export function simulate(tasks: Task[], iterations: number): SimulateResult {
+  let total = 0
+  let breakdown: Breakdown = {}
+  const totalTimes: number[] = []
+
   for (let i = 0; i < iterations; i++) {
-    const result = simulateOnce(tasks)
-    results.push(result)
+    const result = simulateToSuccess(tasks)
+    total += result.totalTime
+    totalTimes.push(result.totalTime)
+    mergeBreakdowns(breakdown, result.breakdown)
   }
 
-  return { total: results.reduce((sum, r) => sum + r, 0) / results.length }
+  const median = findMedian(totalTimes)
+  const mean = total / totalTimes.length
+  const mode = calculateMode(totalTimes)
+  const max = totalTimes.reduce((acc, i) => i > acc ? i : acc, 0)
+
+  const result = { mean, median, mode, max, iterations, tasks }
+  console.log(result)
+  return result
 }
 
-function simulateOnce(tasks: Task[]) {
-  let completed = false;
+function mergeBreakdowns(lhs: Breakdown, rhs: Breakdown) {
+  for (const key in Object.keys(rhs)) {
+    if (!lhs[key]) {
+      lhs[key] = rhs[key]
+    } else {
+      lhs[key] += rhs[key]
+    }
+  }
+}
+function calculateMode(arr: number[]) {
+  let mode: number = NaN;
+  let count = 0;
+  const dict = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    dict[num] = dict[num] ? dict[num] + 1 : 1;
+    if (dict[num] > count) {
+      mode = num;
+      count = dict[num];
+    }
+  }
+
+  return mode;
+}
+
+export function findMedian(dataSet: number[]) {
+  const numbers = [...dataSet]
+  //chat gpt
+  // sort the array in ascending order
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  var length = numbers.length;
+  var middleIndex = Math.floor(length / 2);
+
+  // check if the array has an even or odd number of elements
+  if (length % 2 === 0) {
+    // if even, return the average of the middle two elements
+    return (numbers[middleIndex - 1] + numbers[middleIndex]) / 2;
+  } else {
+    // if odd, return the middle element
+    return numbers[middleIndex];
+  }
+}
+
+interface SimulateOnceResult {
+  totalTime: number
+  breakdown: Breakdown
+}
+function simulateToSuccess(tasks: Task[]): SimulateOnceResult {
+  let completed = false
   let total = 0
+  let breakdowns: Breakdown = {}
   while (!completed) {
-    const { success, time } = simulateLoop(tasks)
+    const { success, time, breakdown } = simulateLoop(tasks)
     total += time
     completed = success
-
+    mergeBreakdowns(breakdowns, breakdown)
   }
 
-  return total
+  return { totalTime: total, breakdown: breakdowns }
 }
 
 interface Result {
   success: boolean
   time: number
+  breakdown: Breakdown
 }
 
 function simulateLoop(tasks: Task[]): Result {
   let time = 0
+  let breakdown: Breakdown = {}
   for (const t of tasks) {
     time += t.time
     let test = Math.random()
-    if (test < t.failure) return { success: false, time }
+    if (!breakdown[t.name]) breakdown[t.name] = 0
+    breakdown[t.name] += t.time
+    if (test < t.failure) return { success: false, time, breakdown }
   }
 
-  return { success: true, time }
+  return { success: true, time, breakdown }
 
 }
