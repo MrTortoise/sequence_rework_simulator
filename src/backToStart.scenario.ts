bd32224b6263ab96e5066fccc0fb1540feba96ae
John Nicholas <themrtortoise@gmail.com>
2024-04-11 08:59:26 +0100

    typo

--- src/backToStart.scenario.ts
+++ src/backToStart.scenario.ts
@@ -2,7 +2,7 @@ import { simulate } from "./backToStartSimulator";
 const iterations = 1000000
 
 describe("scenarios for back to start simulator", ()=>{
-    test('3 tasks task will a suprining amount of time', () => {
+    test('3 tasks task will a surprising amount of time', () => {
         const tasks = [{ time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0.5, name: 'test' }, { time: 100, failure: 0.5, name: 'nearest' }]//, { time: 100, failure: 0.5 }]
         const result = simulate('3 tasks,100 duration, 2+3 have failure rate of 50%', tasks, iterations)
         expect(result.mean).toBeGreaterThan(995);
@@ -13,7 +13,7 @@ describe("scenarios for back to start simulator", ()=>{
       })
     
       
-      test('3 tasks task with only QA failure a suprining amount of time', () => {
+      test('3 tasks task with only QA failure a surprising amount of time', () => {
         const tasks = [{ time: 100, failure: 0, name: 'product' }, { time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0.5, name: 'QA' }]//, { time: 100, failure: 0.5 }]
         const result = simulate('3 tasks,100 duration, 3 has failure rate of 50%', tasks, iterations)
         expect(result.mean).toBeGreaterThan(598);
@@ -23,7 +23,7 @@ describe("scenarios for back to start simulator", ()=>{
         expect(result.mode).toBe(300);
       })
     
-      test('4 tasks task will a suprining amount of time', () => {
+      test('4 tasks task will a surprising amount of time', () => {
         const tasks = [{ time: 100, failure: 0, name: 'dev' }, { time: 100, failure: 0.5, name: 'test' }, { time: 100, failure: 0.5, name: 'nearest' }, { time: 100, failure: 0.5, name: 'int' }]
         const result = simulate('4 tasks: dev, test, qa, int, 100 duration - 50% failure', tasks, iterations)
         expect(result.mean).toBeGreaterThan(2195);
@@ -32,7 +32,7 @@ describe("scenarios for back to start simulator", ()=>{
         expect(result.mode).toBe(400);
       })
     
-      test('2 tasks reversed  will a  less suprining amount of time', () => {
+      test('2 tasks reversed  will a  less surprising amount of time', () => {
         const tasks = [{ time: 100, failure: 0.5, name: 'dev' }, { time: 100, failure: 0, name: 'test' }]//, { time: 100, failure: 0.5 }, { time: 100, failure: 0.5 }]
         const result = simulate('2 tasks, first  (50%) second does not. 100 duration', tasks, iterations)
         expect(result.mean).toBeGreaterThan(295);
@@ -42,7 +42,7 @@ describe("scenarios for back to start simulator", ()=>{
         expect(result.mode).toBe(200);
       })
     
-      test('4 tasks first reversed rest halved  will a suprining amount of time', () => {
+      test('4 tasks first reversed rest halved  will a surprising amount of time', () => {
         const tasks = [{ time: 100, failure: 0.5, name: 'dev' }, { time: 100, failure: 0, name: 'qa' }, { time: 100, failure: 0.25, name: 'int' }, { time: 100, failure: 0.25, name: 'uat' }]
         const result = simulate('4 tasks, dev (50%), qa (0%), int (25%), uat (25%)', tasks, iterations)
         expect(result.mean).toBeGreaterThan(843);
